43e4810560487feeaa33b3bff734e1e3b4cb44b3
Pascal Jungblut <mail@pascalj.de>
2023-10-02 17:32:41 +0200

    Nix fmt

--- flake.nix
+++ flake.nix
@@ -9,32 +9,34 @@
     };
   };
 
-  outputs = { self, nixpkgs, homeManager }: let
-    stateVersion = "23.05";
-    system = "x86_64-linux";
-    hosts = [
-      { username = "pascalj"; hostname = "carol"; }
-      { username = "pascal"; hostname = "GS"; }
-    ];
+  outputs = { self, nixpkgs, homeManager }:
+    let
+      stateVersion = "23.05";
+      system = "x86_64-linux";
+      hosts = [
+        { username = "pascalj"; hostname = "carol"; }
+        { username = "pascal"; hostname = "GS"; }
+      ];
 
-    pkgs = nixpkgs.legacyPackages.${system};
-    lib = pkgs.lib;
-    mkHome = {username, hostname}: {
-      "${username}@${hostname}" = homeManager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
+      mkHome = { username, hostname }: {
+        "${username}@${hostname}" = homeManager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = (builtins.filter builtins.pathExists [
             ./home.nix
             ./${hostname}.nix
           ]) ++ [
             {
-            home.username = username;
-            home.homeDirectory = "/home/${username}";
-          }
-        ];
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+            }
+          ];
         };
+      };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+      homeConfigurations = lib.attrsets.mergeAttrsList (map mkHome hosts);
     };
-  in {
-    formatter.${system} = pkgs.nixpkgs-fmt;
-    homeConfigurations = lib.attrsets.mergeAttrsList (map mkHome hosts);
-  };
 }
