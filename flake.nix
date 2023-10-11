860c2f97342ec424d20e33007a3008c3d8b7635c
Pascal Jungblut <mail@pascalj.de>
2023-10-12 00:24:01 +0200

    Cleanup

--- flake.nix
+++ flake.nix
@@ -3,42 +3,45 @@
 
   inputs = {
     nixpkgs.url = "flake:nixpkgs";
-    homeManager = {
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, homeManager }:
-    let
-      stateVersion = "23.05";
-      system = "x86_64-linux";
-      # 'home-manager switch' will look for <username>@<host> and <username>
-      hosts = [
-        { username = "pascalj"; hostname = "carol"; }
-        { username = "pascal"; hostname = "GS-3KXV8Y3"; }
-      ];
-
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
-      # Small wrapper to have a ./<hostname>.nix file
-      mkHome = { username, hostname }: {
-        "${username}@${hostname}" = homeManager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = (builtins.filter builtins.pathExists [
-            ./home.nix
-            ./${hostname}.nix
-          ]) ++ [
-            {
-              home.username = username;
-              home.homeDirectory = "/home/${username}";
-            }
-          ];
+  outputs = { self, nixpkgs, home-manager, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      };
-    in
-    {
-      formatter.${system} = pkgs.nixpkgs-fmt;
-      homeConfigurations = lib.attrsets.mergeAttrsList (map mkHome hosts);
-    };
+        # 'home-manager switch' will look for <username>@<host> and <username>
+        hosts = [
+          { username = "pascalj"; hostname = "carol"; }
+          { username = "pascal"; hostname = "GS-3KXV8Y3"; }
+        ];
+
+        # Small wrapper to have a ./<hostname>.nix file
+        mkHome = { username, hostname }: {
+          "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = (builtins.filter builtins.pathExists [
+              ./home.nix
+              ./${hostname}.nix
+            ]) ++ [
+              {
+                home.username = username;
+                home.homeDirectory = "/home/${username}";
+              }
+            ];
+          };
+        };
+
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages.homeConfigurations = pkgs.lib.attrsets.mergeAttrsList (map mkHome hosts);
+      }
+    );
 }
--- home.nix
+++ home.nix
@@ -1,21 +1,11 @@
 { config, pkgs, lib, ... }:
 {
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
+  fonts.fontconfig.enable = true;
+
+  home.file = import ./home/dotfiles {
+    inherit pkgs lib;
+  };
   home.stateVersion = "23.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-
   home.packages = with pkgs; [
     bat
     fd
@@ -27,39 +17,41 @@
     tree
   ];
 
-  fonts.fontconfig.enable = true;
+  news.display = "silent";
 
-  # Programs
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.git = {
-    enable = true;
-    userName = "Pascal Jungblut";
-    userEmail = "mail@pascalj.de";
-    ignores = [ ".lvimrc" ];
-    aliases = {
-      st = "status";
-      co = "checkout";
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
     };
-    diff-so-fancy.enable = true;
-  };
-  programs.jq.enable = true;
-  programs.neovim = import ./home/neovim {
-    inherit pkgs lib;
-  };
-  programs.rofi = {
-    enable = true;
-    plugins = [ pkgs.rofi-calc ];
-    theme = ./home/rofi/nord.rasi;
-  };
-  programs.zsh = import ./home/zsh {
-    inherit config lib pkgs;
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+    git = {
+      enable = true;
+      userName = "Pascal Jungblut";
+      userEmail = "mail@pascalj.de";
+      ignores = [ ".lvimrc" ];
+      aliases = {
+        st = "status";
+        co = "checkout";
+      };
+      diff-so-fancy.enable = true;
+    };
+    home-manager.enable = true;
+    jq.enable = true;
+    neovim = import ./home/neovim {
+      inherit pkgs lib;
+    };
+    rofi = {
+      enable = true;
+      plugins = [ pkgs.rofi-calc ];
+      theme = ./home/rofi/nord.rasi;
+    };
+    zsh = import ./home/zsh {
+      inherit config lib pkgs;
+    };
   };
 
-  # dotfiles
-  home.file = import ./home/dotfiles {
-    inherit pkgs lib;
-  };
 }
